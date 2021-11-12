36afa4e602e3244d300da7ed97e382e925eff525
Vaszil Ádám <adam.vaszil@wup.hu>
2021-11-12 11:41:10 +0100

    Fix jitpack issue

--- app/src/main/java/hu/wup/generatedapi/api/ApiServiceRx.kt
+++ app/src/main/java/hu/wup/generatedapi/api/ApiServiceRx.kt
@@ -1,11 +1,11 @@
 package hu.wup.generatedapi.api
 
 import hu.wup.generatedapi.model.CardEntityResponse
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
-import rx.Single
 
 interface ApiServiceRx {
 
     @GET("cards.json")
-    fun getCardData(): Single<ArrayList<CardEntityResponse>>
+    fun getCardData(): Single<List<CardEntityResponse>>
 }
